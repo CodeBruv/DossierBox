7d01b2b842f78e2b0db348022d84f29fccaef9c0
Code Bruv <abdooolmajeeed@gmail.com>
2026-08-18 09:04:34 +0100

    Adjust vitest.config.ts [2026-08-18 09:04:34]

---
+++ vitest.config.ts
@@ -0,0 +1,36 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    testTimeout: 30_000,
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@/auth",
+        replacement: resolve(__dirname, "src/auth"),
+      },
+      {
+        find: "@/profile",
+        replacement: resolve(__dirname, "src/profile"),
+      },
+      {
+        find: "@/ui",
+        replacement: resolve(__dirname, "src/ui"),
+      },
+      {
+        find: "@/lib",
+        replacement: resolve(__dirname, "src/lib"),
+      },
+      {
+        find: "@/config",
+        replacement: resolve(__dirname, "src/config"),
+      },
+      {
+        find: "@",
+        replacement: resolve(__dirname, "."),
+      },
+    ],
+  },
+});
