2cc6a1b0aefc85524a1f19aaf087b8a4b48065e6
Code Bruv <abdooolmajeeed@gmail.com>
2026-08-29 19:08:55 +0100

    Fix issue in 0016_nosy_brood.sql [2026-08-29 19:08:53]

---
+++ drizzle/0016_nosy_brood.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "document_versions" ADD COLUMN "compilerFingerprint" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "document_versions" ADD COLUMN "configurationFingerprint" text NOT NULL;
